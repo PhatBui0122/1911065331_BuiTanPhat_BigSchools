191d2e30f52b3a17864e85d888c5075c6d1c5837
Phat Bui <babylala145@gmail.com>
2022-03-21 23:51:19 +0700

    Add project files.

---
+++ 1911065331_BuiTanPhat_BigSchools/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911065331_BuiTanPhat_BigSchools.MvcApplication" Language="C#" %>
---
+++ 1911065331_BuiTanPhat_BigSchools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1911065331_BuiTanPhat_BigSchools.Startup))]
+namespace _1911065331_BuiTanPhat_BigSchools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
